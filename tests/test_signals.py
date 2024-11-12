541c0d73b5ae34a1e32283ca6935b6759d20fb41
Tao Bojlén <66130243+taobojlen@users.noreply.github.com>
2024-11-12 12:07:26 +0000

    chore: small fixes

--- tests/test_signals.py
+++ tests/test_signals.py
@@ -1,8 +1,11 @@
+import warnings
+
 import pytest
 import pytest_django
 import pytest_mock
 from djangoproject.social import models
 from zeal import errors
+from zeal.listeners import n_plus_one_listener
 
 from . import factories
 
@@ -22,9 +25,12 @@ def test_signal_send_message(
     user_1, user_2 = factories.UserFactory.create_batch(2)
     factories.PostFactory.create(author=user_1)
     factories.PostFactory.create(author=user_2)
-    _ = [
-        post.author.username
-        for post in models.Post.objects.all()
-    ]
+    with warnings.catch_warnings(record=True):
+        warnings.simplefilter("always")
+        _ = [post.author.username for post in models.Post.objects.all()]
     patched_signal.assert_called_once()
-
+    sender = patched_signal.call_args[1]["sender"]
+    assert sender == n_plus_one_listener
+    exception = patched_signal.call_args[1]["exception"]
+    assert isinstance(exception, errors.NPlusOneError)
+    assert "N+1 detected on social.Post.author" in str(exception)
